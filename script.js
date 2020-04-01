31179bae5df8a984db0c484fe6612df0e621555b
IhtishamMushtaq <musaibmushtaqq@gmail.com>
2020-04-01 17:48:57 +0530

    Animations added

---
+++ script.js
@@ -0,0 +1,5 @@
+function hit() {
+  document.getElementsByClassName("span1")[0].setAttribute("id", "new-span1");
+  document.getElementsByClassName("span2")[0].setAttribute("id", "new-span2");
+  document.getElementsByClassName("span3")[0].setAttribute("id", "new-span3");
+}
